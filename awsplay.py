6173ec43104f33e5ffead809f7ba1f178f7cc62c
rahulhegde <hegde.rahul@gmail.com>
2020-05-27 13:37:22 -0400

    Provide delete_secret and snapshot

--- awsplay.py
+++ awsplay.py
@@ -28,6 +28,11 @@ def create_secret():
     except Exception as exp:
         print "error: ", exp
     else:
+    # create secret:  {'ResponseMetadata': {'RetryAttempts': 0, 'HTTPStatusCode': 200, 'RequestId': '1b137688-2303-4f7d-a6b7-17d7ba447d44', 
+    # 'HTTPHeaders': {'date': 'Wed, 27 May 2020 17:18:59 GMT', 'x-amzn-requestid': '1b137688-2303-4f7d-a6b7-17d7ba447d44', 
+    # 'content-length': '145', 'content-type': 'application/x-amz-json-1.1', 'connection': 'keep-alive'}}, 
+    # u'VersionId': u'a2b8bfac-4dee-47d2-b069-e1d1677518e5', u'Name': u'secret3', 
+    # u'ARN': u'arn:aws:secretsmanager:us-east-2:474711206021:secret:secret3-FD5Qis'}
         print "create secret: ", response
 
 def get_secret():
@@ -132,15 +137,59 @@ def update_secret():
     except Exception as exception:
         print "exception: ", exception 
     else:
+    # update secret:  {'ResponseMetadata': {'RetryAttempts': 0, 'HTTPStatusCode': 200, 'RequestId': '5bcd06b1-f3b5-4fd1-b4d8-219e14bec908', 
+    # 'HTTPHeaders': {'date': 'Wed, 27 May 2020 17:22:16 GMT', 
+    # 'x-amzn-requestid': '5bcd06b1-f3b5-4fd1-b4d8-219e14bec908', 
+    # 'content-length': '145', 'content-type': 'application/x-amz-json-1.1', 'connection': 'keep-alive'}}, 
+    # u'VersionId': u'86638a4e-3796-4567-90fa-a259760a87a8', u'Name': u'secret3', 
+    # u'ARN': u'arn:aws:secretsmanager:us-east-2:474711206021:secret:secret3-FD5Qis'}
         print "update secret: ", get_secret_value_response
-                
+
     # Your code goes here. 
+
+
+def delete_secret():
+
+    secret_name = "secret3"
+    region_name = "us-east-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/secretsmanager.html#SecretsManager.Client.delete_secret    # SecretsManager.Client.exceptions.InvalidParameterException
+    try:
+        get_secret_value_response = client.delete_secret(
+            SecretId=secret_name,
+            ForceDeleteWithoutRecovery=True)
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            #raise e
+            print "secret not found: ", e
+            #raise e
+    except Exception as exception:
+        print "exception: ", exception 
+    else:
+        # delete secret:  {'ResponseMetadata': {'RetryAttempts': 0, 'HTTPStatusCode': 200, 'RequestId': '7399de77-be79-441d-9f1d-405aca18fde2', 
+        # 'HTTPHeaders': {'date': 'Wed, 27 May 2020 17:33:39 GMT', 'x-amzn-requestid': '7399de77-be79-441d-9f1d-405aca18fde2', 
+        # 'content-length': '126', 'content-type': 'application/x-amz-json-1.1', 'connection': 'keep-alive'}}, 
+        # u'DeletionDate': datetime.datetime(2020, 5, 27, 13, 33, 39, 481000, tzinfo=tzlocal()), 
+        # u'Name': u'secret3', u'ARN': u'arn:aws:secretsmanager:us-east-2:474711206021:secret:secret3-FD5Qis'}
+        print "delete secret: ", get_secret_value_response
+                
+    # Your code goes here.
 
 
 
 #create_secret()
 #get_secret()
-update_secret()
+#update_secret()
+delete_secret()
 
 # import boto3
 # from botocore.exceptions import ClientError
